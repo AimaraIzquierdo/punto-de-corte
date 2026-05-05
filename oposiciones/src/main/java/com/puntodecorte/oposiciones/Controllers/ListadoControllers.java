417e0b97331aafb84db24a64c58a60823c05b909
Aimara <aimara.92@gmail.com>
2026-05-05 21:19:34 +0200

    Refactorización del código, preparación de la bd para acceso

--- oposiciones/src/main/java/com/puntodecorte/oposiciones/Controllers/ListadoControllers.java
+++ oposiciones/src/main/java/com/puntodecorte/oposiciones/Controllers/ListadoControllers.java
@@ -1,13 +1,26 @@
 package com.puntodecorte.oposiciones.Controllers;
 
+import com.puntodecorte.oposiciones.Dominio.modulos;
+import com.puntodecorte.oposiciones.Service.modulosService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ListadoControllers {
+
+    private final modulosService modulosService;
+
+    public ListadoControllers(modulosService modulosService) {
+        this.modulosService = modulosService;
+    }
+
     @RequestMapping ("/")
-    public String listarModulos(){//Cuando el ordenador acceda a la URL se activa este método, que redirigirá a la página de listado.
-        //Busqueda de los módulos accesibles
+    public String listarModulos(Model model){
+        List<modulos> modulos = modulosService.listarModulos();
+        model.addAttribute("modulos", modulos);
         return "Listado";
     }
 }
