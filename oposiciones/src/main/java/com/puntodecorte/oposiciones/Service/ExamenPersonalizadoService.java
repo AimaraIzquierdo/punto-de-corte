a7053e10acc97bad1438af1d42b14fba0da08d70
Diana Rodriguez <diana.rodriguez.isabel06@gmail.com>
2026-05-29 16:15:36 +0200

    Arreglo examenes personalizados

--- oposiciones/src/main/java/com/puntodecorte/oposiciones/Service/ExamenPersonalizadoService.java
+++ oposiciones/src/main/java/com/puntodecorte/oposiciones/Service/ExamenPersonalizadoService.java
@@ -1,8 +1,6 @@
 package com.puntodecorte.oposiciones.Service;
 
-import com.puntodecorte.oposiciones.Dominio.CategoriaExamen;
 import com.puntodecorte.oposiciones.Dominio.PreguntaPersonalizada;
-import com.puntodecorte.oposiciones.Repository.CategoriaExamenRepository;
 import com.puntodecorte.oposiciones.Repository.PreguntaPersonalizadaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
@@ -14,32 +12,15 @@ import java.util.List;
 @SessionScope
 public class ExamenPersonalizadoService {
 
-    private final CategoriaExamenRepository categoriaRepo;
     private final PreguntaPersonalizadaRepository preguntaRepo;
 
     private final List<PreguntaPersonalizada> buffer = new ArrayList<>();
-    private CategoriaExamen categoriaSeleccionada;
 
-    public ExamenPersonalizadoService(CategoriaExamenRepository categoriaRepo,
-                                      PreguntaPersonalizadaRepository preguntaRepo) {
-        this.categoriaRepo = categoriaRepo;
+    public ExamenPersonalizadoService(PreguntaPersonalizadaRepository preguntaRepo) {
         this.preguntaRepo = preguntaRepo;
     }
 
-    public List<CategoriaExamen> listarCategorias() {
-        return categoriaRepo.findAll();
-    }
-
-    public void setCategoria(CategoriaExamen categoria) {
-        this.categoriaSeleccionada = categoria;
-    }
-
     public void guardarPregunta(PreguntaPersonalizada pregunta) {
-
-        if (categoriaSeleccionada != null) {
-            pregunta.setCategoria(categoriaSeleccionada);
-        }
-
         buffer.add(pregunta);
         preguntaRepo.save(pregunta);
     }
@@ -55,11 +36,6 @@ public class ExamenPersonalizadoService {
     public List<PreguntaPersonalizada> generarExamen() {
         List<PreguntaPersonalizada> examen = new ArrayList<>(buffer);
         buffer.clear();
-        categoriaSeleccionada = null;
         return examen;
-    }
-    // Devuelve el buffer SIN limpiarlo (para usar en corrección)
-    public List<PreguntaPersonalizada> obtenerBuffer() {
-        return new ArrayList<>(buffer);
     }
 }
